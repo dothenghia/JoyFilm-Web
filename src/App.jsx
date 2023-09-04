5b1b86cf440e5b90fa357cb2f943b22d0075cb4c
Thế Nghĩa <nghiask2018@gmail.com>
2023-09-04 15:15:01 +0700

    Setup project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+
+const App = () => {
+    return (
+        <div>
+            <Button variant="contained">Hello world</Button>
+            <p>123</p>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+import "@fontsource/roboto/100.css"
+import "@fontsource/roboto/300.css"
+import "@fontsource/roboto/400.css"
+import "@fontsource/roboto/500.css"
+import "@fontsource/roboto/700.css"
+import "@fontsource/roboto/900.css"
+import './main.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+)
