0cf556942cde519d95caaeec6c0b5ef9ecf70547
Thế Nghĩa <nghiask2018@gmail.com>
2023-09-13 23:39:39 +0700

    Build NotFound Page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,18 @@
+import { Link } from "react-router-dom";
+
 
 const NotFound = () => {
     console.log('[NotFound] Render')
 
     return (
-        <div style={{backgroundColor: 'pink'}}>
-            <h1>NotFound page</h1>
+        <div id="notfound" className="page-container-0topside">
+            <div className="section-container">
+                <h1 className="section-title text-center">Trang không tồn tại</h1>
+
+                <Link to='/' className="episode-btn">
+                    Quay lại trang chính
+                </Link>
+            </div>
         </div>
     );
 };
