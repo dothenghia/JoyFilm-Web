cf8f61e0f0ad5185f73110687b7244ed470faf52
Thế Nghĩa <nghiask2018@gmail.com>
2023-09-18 16:24:06 +0700

    Build Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,34 @@
+import logo from '../../assets/logo.png';
+import joyfilm from '../../assets/joyfilm.png';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <div id="footer">
-            <h1 className="py-10 bg-stone-950"></h1>
+        <div className='bg-stone-950 pt-6 pb-10 sm:py-8'>
+            <div className='section-container-no-py flex flex-col-reverse sm:flex-row items-center justify-between'>
+                <Link to="/" className="flex items-center mt-8 sm:mt-0">
+                    <img src={logo} className="h-11 mr-2" alt="Logo" />
+                    <img src={joyfilm} className="h-10" alt="JoyFilm" />
+                </Link>
+                <div className='flex w-full justify-evenly sm:w-auto items-center'>
+                    <Link className='footer-link'>
+                        Giới thiệu
+                    </Link>
+                    <Link to='https://github.com/dothenghia/JoyFilm-Web'
+                        target='_blank'
+                        className='footer-link'>
+                        Github
+                    </Link>
+                    <Link to='https://github.com/dothenghia/JoyFilm-Server'
+                        target='_blank'
+                        className='footer-link'>
+                        API
+                    </Link>
+                    <Link className='footer-link'>
+                        Liên hệ
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
